798504fdc00244f9899fccb29fc0861b48f8be38
Jeff Gubbins <jeff@jgubbins.com>
2025-01-17 00:02:53 -0500

    Initial code

--- readFid_Visa.py
+++ readFid_Visa.py
@@ -1,17 +1,6 @@
-# readFidelity.py  reads in excel sheet
+# readFid_Visa.py  reads in excel sheet
 import pandas as pd
 import outlay_func as of
-
-
-
-# open db outlay
-db = of.connect_to_db()
-cursor = db.cursor()
-
-
-
-
-
 
 # open 2024 Fidelity Visa activity
 filename = r"/Users/jeff/Downloads/Credit Card - 8869_01-01-2024_12-31-2024.csv"
@@ -20,10 +9,16 @@ df = pd.read_csv(filename)
 # clean file
 df = of.remove_null_rows(df, 'Name')
 
+# keep only Date, Name, Amount fields
 columns_to_keep = ['Date', 'Name', 'Amount']
 df = df.drop(columns=[col for col in df.columns if col not in columns_to_keep])
 
-    # date_obj = of.datestr2date(date_str)
+# open db outlay
+db = of.connect_to_db()
+cursor = db.cursor()
+
+# get a list of dates already in the table
+datesNtable = of.get_loaded_dates(cursor, "date", "fid_visa")
 
 # Step through the DataFrame one row at a time
 for index, row in df.iterrows():
@@ -32,18 +27,16 @@ for index, row in df.iterrows():
     name = row['Name']
     amount = row['Amount']
 
-
-    # Check if the row exists
-    check_query = "SELECT COUNT(*) FROM fid_visa WHERE date = %s AND vendor = %s and Amount = %s"
-    cursor.execute(check_query, (date_obj, name, amount))
-    result = cursor.fetchone()
-
-    insert_query = "INSERT INTO fid_visa (date, vendor, amount) VALUES (%s, %s, %s)"
-    cursor.execute(insert_query, (date_obj, name, amount))
-
-    # Commit every 25 entries
-    if index % 25 == 0:
-        db.commit()
-        print("commit")
+    # exclude rows with dates already in the table
+    if date_str.strip() not in datesNtable:
+        insert_query = "INSERT INTO fid_visa (date, vendor, amount) VALUES (%s, %s, %s)"
+        cursor.execute(insert_query, (date_obj, name, amount))
+        # Commit every 25 entries
+        if index % 25 == 0:
+            db.commit()
 
 db.commit()
+# Closing the cursor
+cursor.close()
+# Closing the connection
+db.close()
