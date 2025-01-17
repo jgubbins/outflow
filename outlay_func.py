ed52dae99507a6816d6311b02477f7ce830abc14
Jeff Gubbins <jeff@jgubbins.com>
2025-01-16 23:38:08 -0500

    Initial code

--- outlay_func.py
+++ outlay_func.py
@@ -26,10 +26,27 @@ def connect_to_db():
 def remove_null_rows(df, column_name):
     return df[df[column_name].notnull()]
 
+# reads a date string, reformats it, and returns a datetime obj
 def datestr2date(date_str):
     # check for 4-digit year, add if necessary
     if date_str[-4:-2] != '20':
         date_str = f'{date_str[:-2]}20{date_str[-2:]}'
+    date_str = date_str.lstrip()
     date_obj = datetime.strptime(date_str, "%m/%d/%Y").date()
     return date_obj
 
+
+# gets distinct dates from a table and returns a list of stromg
+def get_loaded_dates(cursor, field, table):
+    # Execute the SQL query to get distinct entries from a column
+
+    cursor.execute(f"SELECT DISTINCT {field} FROM {table}")
+    # Fetch all distinct values and insert them into a list
+    distinct_values = ( [row[0] for row in cursor.fetchall()])
+
+    justDate = []
+    for i in range(len(distinct_values)):
+        dv = distinct_values[i].strftime("%m-%d-%Y")
+        dv = dv.replace('-', '/')
+        justDate.append(dv)
+    return justDate
--- readFid_Chking.py
+++ readFid_Chking.py
@@ -1,50 +1,44 @@
 # readFid_Chkng.py  reads in checking account statements
-
 import pandas as pd
 import outlay_func as of
 
+# open Fidelity Checking acc't activity
+filename = r"/Users/jeff/Downloads/History_for_Account_X64898317-2.csv"
+df = pd.read_csv(filename)
 
+# clean file
+df = of.remove_null_rows(df, 'Action')
+# Reverse the order of the DataFrame
+df = df.iloc[::-1]
+
+# keep only Run Date, Action,Amount ($) fields
+columns_to_keep = ['Run Date', 'Action', 'Amount ($)']
+df = df.drop(columns=[col for col in df.columns if col not in columns_to_keep])
 
 # open db outlay
 db = of.connect_to_db()
 cursor = db.cursor()
 
+# get a list of dates already in the table
+datesNtable = of.get_loaded_dates(cursor, "date", "fid_chkg")
 
-
-
-
-
-# open 2024 Fidelity Visa activity
-filename = r"/Users/jeff/Downloads/Credit Card - 8869_01-01-2024_12-31-2024.csv"
-df = pd.read_csv(filename)
-
-# clean file
-df = of.remove_null_rows(df, 'Name')
-
-columns_to_keep = ['Date', 'Name', 'Amount']
-df = df.drop(columns=[col for col in df.columns if col not in columns_to_keep])
-
-    # date_obj = of.datestr2date(date_str)
-
-# Step through the DataFrame one row at a time
+#Step through the DataFrame one row at a time
 for index, row in df.iterrows():
-    date_str = row['Date']
+    date_str = row['Run Date']
     date_obj = of.datestr2date(date_str)
-    name = row['Name']
-    amount = row['Amount']
+    action = row['Action']
+    amount = row['Amount ($)']
 
-
-    # Check if the row exists
-    check_query = "SELECT COUNT(*) FROM fid_visa WHERE date = %s AND vendor = %s and Amount = %s"
-    cursor.execute(check_query, (date_obj, name, amount))
-    result = cursor.fetchone()
-
-    insert_query = "INSERT INTO fid_visa (date, vendor, amount) VALUES (%s, %s, %s)"
-    cursor.execute(insert_query, (date_obj, name, amount))
-
-    # Commit every 25 entries
-    if index % 25 == 0:
-        db.commit()
-        print("commit")
+    # exclude rows with dates already in the table
+    if date_str.strip() not in datesNtable:
+        insert_query = "INSERT INTO fid_chkg (date, action, amount) VALUES (%s, %s, %s)"
+        cursor.execute(insert_query, (date_obj, action, amount))
+        # Commit every 25 entries
+        if index % 25 == 0:
+            db.commit()
 
 db.commit()
+# Closing the cursor
+cursor.close()
+# Closing the connection
+db.close()
